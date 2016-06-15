4129a356702df764dbdad3ec965503c658accd60
Fernando Pozo <fernanpozo@gmail.com>
2016-06-15 13:26:05 +0200

    Update Ratt2Coords.sh

--- Ratt2Coords.sh
+++ Ratt2Coords.sh
@@ -1,12 +1,5 @@
 # RATT USAGE --> ratt embl .fna annotated Assembly
 
-# default values
-PRGNAME=`basename $0`
-RATT_MODE='Assembly'
-REF_EMBL_FILE='reference.embl'
-QUERY_FASTA_FILE='unannotated.fasta'
-
-
 # RATT Parameters:
 #	1- embl folder containing .embl file reference
 #	2- query fasta (.fna) file (in working dir) containing contigs to be annotated
@@ -17,8 +10,8 @@ QUERY_FASTA_FILE='unannotated.fasta'
 function usage()
 {
 	echo 'To call the program, type the following in the command line:'
-	echo "bash $PRGNAME -m RATT_mode -r Reference_file.embl -i Query_file.fasta"
-	echo 'RATT modes implemented: Assembly'
+	echo 'bash Ratt2Coords.sh -r Reference_file.embl -i Query_file.fasta -m RATT_mode'
+	echo 'RATT modes implemented: Assembly, Strain, Species'
 
 }
 
@@ -47,54 +40,18 @@ function quickcp()
     fi
 }
 
+if [ $# -ne 6 ]; then echo "Wrong number of arguments supplied!!"; usage; exit; fi
+if [ $1 == "-r" ]; then REF_EMBL_FILE=$2; echo "$REF_EMBL_FILE"; fi
+if [ $3 == "-i" ]; then QUERY_FASTA_FILE=$4; echo "$QUERY_FASTA_FILE"; fi
+if [ $5 == "-m" ]; then RATT_MODE=$6; echo "$RATT_MODE"; fi
 
-if [ $# -eq 1 ] ; then usage ; fi
-
-TEMP=`getopt -o r:m:i:hv \
-     --long reference:,method:,input:,help,verbose \
-     -n "$0" -- "$@"`
-
-# an invalid option was given
-if [ $? != 0 ] ; then usage >&2 ; exit 1 ; fi
-
-# Note the quotes around `$TEMP': they are essential!
-#  the set command takes any arguments after the options (-- signals the end
-#  of the options) and assigns them to positional parameters $0..$n.
-#  We pass set through eval so that bash will honor embedded quotes in the
-#  string, i.e.
-#	whithout eval, "ab cd" is split at the space into "ab and cd"
-#	with eval, bash honors que quotes and gives "ab cd" as a single value
-#
-eval set -- "$TEMP"
-
-while true ; do
-        case "$1" in
-                -h|--help) 
-                    usage ; shift ;;
-
-                -i|--input)
-                    QUERY_FASTA_FILE="$2" ; shift 2 ;;
-                    
-                -r|--reference)
-                    REF_EMBL_FILE="$2" ; shift 2 ;;
-                    
-		-m|--method)
-		    RATT_MODE="$2" ; shift 2 ;;
-		    
-                -v|--verbose)
-		   VERBOSE='yes' ; shift ;;
-                --) shift ; break ;;
-                *) echo "$0: Internal error!" >&2 ; usage ; exit 1 ;;
-        esac
-done
-
-
-if [ $RATT_MODE != 'Assembly' ]; then echo 'Only Assembly mode is implemented in current version'; exit; fi
+# if [ $RATT_MODE != 'Assembly' ]; then echo 'Only Assembly mode is implemented in current version'; exit; fi
 
 echo "Would you like to erase all files created by RATT after program execution? (y/n) --> "
 read erase 
 
-TRIAL_DIR="Ratt2Coords"
+CURRENT_DIR=`pwd`
+TRIAL_DIR="trial_dir"
 mkdir -p $TRIAL_DIR
 quickcp $REF_EMBL_FILE $TRIAL_DIR
 quickcp $QUERY_FASTA_FILE $TRIAL_DIR
@@ -102,10 +59,10 @@ quickcp ReadCoords003.py $TRIAL_DIR
 cp ratt/* $TRIAL_DIR
 cd $TRIAL_DIR
 
-RATT_HOME=`pwd`
+RATT_HOME=$CURRENT_DIR/$TRIAL_DIR
 export RATT_HOME
 
-if [ ! $RATT_MODE ]; then RATT_MODE="Assembly"; fi
+# if [ ! $RATT_MODE ]; then RATT_MODE="Assembly"; fi
 
 EMBL_dir="embl"
 if [ ! -d $EMBL_dir ] ; then mkdir -p $EMBL_dir ; fi
@@ -126,6 +83,3 @@ quickcp $EXCEL_FILE ..
 echo "$EXCEL_FILE copied back to working directory"
 cd ..
 if [ $erase == 'y' ]; then rm -rf $TRIAL_DIR; fi 
-
-
-
